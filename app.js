081d57ead3504a48234878c40613a651114d8dce
TonyMontana143 <221074@astanait.edu.kz>
2024-01-24 17:37:17 +0600

    task is done

---
+++ app.js
@@ -0,0 +1,29 @@
+const { Web3 } = require('web3');
+const fs = require('fs');
+
+
+const infuraApiKey = '0a8b302fca724177ab0bcf1693f0aeb4';
+const web3 = new Web3(`https://sepolia.infura.io/v3/0a8b302fca724177ab0bcf1693f0aeb4`, { timeout: 60000 });
+
+
+const contractAddress = '0x6c9a5f4ab0901de2b9f489b48d31a64af15cd63e';
+
+// Read ABI from JSON file
+const abiFilePath = 'ABI.json';
+const abiRawData = fs.readFileSync(abiFilePath);
+const abi = JSON.parse(abiRawData);
+
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+const userAddress = '0xBac57d95b9FE32459E2167F0Ee69cBbE28109fBb';
+
+
+// Example 1: Check balance of the user
+contract.methods.balanceOf(userAddress).call()
+.then(balance => {
+        console.log(`Balance of ${userAddress}: ${balance} tokens`);
+    })
+    .catch(error => {
+        console.error('Error checking balance:', error);
+    });
+
